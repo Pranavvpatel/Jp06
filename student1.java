970421fff6132bcc8c4e3dc1711ca452ac80233b
Pranavvpatel <166048978+Pranavvpatel@users.noreply.github.com>
2024-05-23 08:19:02 +0530

    Add files via upload

---
+++ student1.java
@@ -0,0 +1,22 @@
+class ab{
+    // private String name;
+    public ab()
+    {
+        System.out.println("Unknown name");
+    }
+
+    public ab(String name)
+    {
+        System.out.println(name);
+    }
+
+}
+
+public class  student1
+{
+    public static void main(String[] args) 
+    {
+
+        ab s1 = new ab();
+    }
+}
